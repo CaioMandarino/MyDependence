ffd06191658d2306dcc18557c8ed1fcda284e00e
Caio <caiomand04@gmail.com>
2025-09-25 14:36:00 -0300

    pronto

---
+++ Sources/MyDependency/MyDependency.swift
@@ -0,0 +1,8 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public class MyDependency {
+    public func work() -> String {
+        return "Hello, MyDependency!"
+    }
+}
